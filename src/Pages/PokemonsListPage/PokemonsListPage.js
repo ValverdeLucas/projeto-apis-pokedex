38a5b8f8a1c4644ebcb28e66354b8db8765c2b29
Lucas Valverde <valverdelucas95@gmail.com>
2024-01-05 20:03:13 -0300

    Pull Request 1 - Teste

--- src/Pages/PokemonsListPage/PokemonsListPage.js
+++ src/Pages/PokemonsListPage/PokemonsListPage.js
@@ -9,8 +9,7 @@ function PokemonListPage() {
             <PokemonCard/>
             <PokemonCard/>
             <PokemonCard/>
-            <PokemonCard/>  
-            <PokemonCard/>
+            <PokemonCard/> 
         </Container>
     )
 }
