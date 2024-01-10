4e320690fa640765beba35090194d11b38187665
Lucas Valverde <valverdelucas95@gmail.com>
2024-01-10 17:52:44 -0300

    Implementação das Routes

--- src/App.js
+++ src/App.js
@@ -4,45 +4,17 @@ import PokemonListPage from "./Pages/PokemonsListPage/PokemonsListPage"
 import PokedexPage from "./Pages/PokedexPage/PokedexPage"
 import PokemonDetailPage from "./Pages/PokemonDetailPage/PokemonDetailPage"
 import { useState } from "react"
+import Router from "./Routes/Router"
 
 function App() {
 
-  const [page, setPage] = useState(1)
+  return (
+    <>
+      <GlobalStyled />
+      <Router />
+    </>
 
-  const changePage = (page) => setPage(page)
-
-  switch (page) {
-    case 1:
-
-      return (
-        <>
-          <GlobalStyled />
-          <Header></Header>
-          <PokemonListPage />
-        </>)
-
-    case 2:
-
-      return (
-        <>
-          <GlobalStyled />
-          <Header></Header>
-          <PokedexPage />
-        </>)
-
-    case 3:
-
-      return (
-        <>
-          <GlobalStyled />
-          <Header></Header>
-          <PokemonDetailPage />
-        </>)
-
-    default:
-      return "Página não encontrada"
-
-  }
+  )
 }
 
 export default App;
--- src/Components/Header/HeaderStyles.js
+++ src/Components/Header/HeaderStyles.js
@@ -1,24 +1,52 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-flex-direction: row;
-border: 1px solid black;
+display: grid;
+grid-template-columns: 33% 33% 33%;
+grid-template-rows: 100%;
 height: 15vh;
 width: 100vw;
-text-align: center;
 justify-content: space-around;
 background-color: white;
+align-content: center;
+justify-items: center;
 `
 
 export const Title = styled.img`
-max-height: 75%;
-align-self: center;
 background-color: white;
+grid-column-start:2;
+grid-column-end:3;
+align-self: center;
 `
 
-export const GoToPokedex = styled.button`
+export const PokedexButton = styled.button`
+height: 30%;
+width: 30%;
+grid-column: 1 / span 1;
+grid-row: 1 / span 1;
+align-self: center;
+`
+
+export const HomeButton = styled.button`
+height: 30%;
+width: 30%;
+grid-column-start:3;
+grid-column-end:4;
+align-self: center;
+`
+
+export const DetailsButton = styled.button`
 height: 30%;
 width: 15%;
+grid-column: 1 / span 1;
+grid-row: 1 / span 1;
+align-self: center;
+`
+
+export const RemoveButton = styled.button`
+height: 30%;
+width: 15%;
+grid-column: 3 / span 1;
+grid-row: 1 / span 1;
 align-self: center;
 `
